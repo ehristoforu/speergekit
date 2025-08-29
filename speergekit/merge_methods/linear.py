251100774dd0e0eb489e0601d2c898b293efa734
Evgeniy Hristoforu <136880927+ehristoforu@users.noreply.github.com>
2025-08-29 11:14:18 +0300

    Update linear

--- speergekit/merge_methods/linear.py
+++ speergekit/merge_methods/linear.py
@@ -21,13 +21,19 @@ class LinearMerge(BaseMergeMethod):
         if base_model:
             raise ValueError("Linear merge does not support a base model.")
 
+        if len(models) > 6:
+            raise ValueError("Linear merge supports up to 6 models.")
+
         # Collect all tensor keys from all models
         tensor_keys = set()
         for model in models:
             tensor_keys.update(model.index.tensor_paths.keys())
 
         merged_state_dict = {}
-        weights = parameters.get("weights", [1.0] * len(models))
+        if "weights" in parameters:
+            weights = parameters["weights"]
+        else:
+            weights = [1.0 / len(models)] * len(models)
 
         for key in tqdm(tensor_keys, desc="Merging tensors"):
             tensors = []
